571f41d041a0e807063c8d277ec1e969535ee071
leochenchiayu <103388599+leochenchiayu@users.noreply.github.com>
2024-02-23 18:12:16 +0800

    Update register.php

--- register.php
+++ register.php
@@ -17,7 +17,7 @@ if (isset($_POST['register'])) {
     // 連接到數據庫
     $conn = new mysqli('localhost', 'your_username', 'your_password', 'your_database');
 
-    // 檢查連接
+    
     if ($conn->connect_error) {
         die("連接失敗: " . $conn->connect_error);
     }
