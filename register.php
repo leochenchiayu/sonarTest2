e8765a19878b66cb8ab5d592bdedf1e94fc1f7e5
leochenchiayu <103388599+leochenchiayu@users.noreply.github.com>
2024-02-23 10:46:43 +0800

    Update register.php

--- register.php
+++ register.php
@@ -12,3 +12,27 @@
     </form>
 </body>
 </html>
+<?php
+if (isset($_POST['register'])) {
+    // 連接到數據庫
+    $conn = new mysqli('localhost', 'your_username', 'your_password', 'your_database');
+
+    // 檢查連接
+    if ($conn->connect_error) {
+        die("連接失敗: " . $conn->connect_error);
+    }
+
+    $username = $conn->real_escape_string($_POST['username']);
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // 對密碼進行加密
+
+    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "新記錄創建成功";
+    } else {
+        echo "錯誤: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
