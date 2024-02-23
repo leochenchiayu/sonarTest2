3e3923059a026cb0c516910aef51f9961d740cfa
leochenchiayu <103388599+leochenchiayu@users.noreply.github.com>
2024-02-23 10:45:53 +0800

    Create register.php

---
+++ register.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>註冊</title>
+</head>
+<body>
+    <h2>註冊表單</h2>
+    <form action="register.php" method="post">
+        用戶名: <input type="text" name="username" required><br>
+        密碼: <input type="password" name="password" required><br>
+        <input type="submit" name="register" value="註冊">
+    </form>
+</body>
+</html>
